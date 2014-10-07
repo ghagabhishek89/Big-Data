d4bef5b82e99667a9d63ad5bd6038fbd8369d4ad
Abhishek Ghag <ghagabhishek89@gmail.com>
2014-10-07 17:02:43 -0400

    Get tweet data and push it into a local file

--- LiveTweet/src/LiveTweet.java
+++ LiveTweet/src/LiveTweet.java
@@ -1,3 +1,15 @@
+/*
+ * Program Name : LiveTweet
+ * Created by : Abhishek Ghag
+ * Created Date : Oct 08 2014
+ * Updated Date : Oct 08 2014
+ * Latest update : Write the recent tweets to the file
+ * Program Description : This program fetches live tweets through tweeter rest api 
+ * and stored the data to ta file in an organised fashion
+ */
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import twitter4j.Status;
@@ -5,22 +17,23 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
-/*
- * Program Name : LiveTweet
- * Created by : Abhishek Ghag
- * Program Description : This program fetches live tweets through tweeter rest api 
- * and stored the data to ta file in an organised fashion
- */
 public class LiveTweet {
 	
-	public static void main(String args[]) throws TwitterException{
+	public static void main(String args[]) throws TwitterException, NumberFormatException, IOException{
+		
 		Twitter twitter = TwitterFactory.getSingleton();
 	    List<Status> statuses = twitter.getHomeTimeline();
 	    System.out.println("Showing home timeline.");
+	    
+	    //create a local file to write the tweets
+	    FileOutputStream fos = new FileOutputStream("recentTweetList.txt");
+	    
 	    for (Status status : statuses) {
-	        System.out.println(status.getUser().getName() + ":" +
-	                           status.getText());
+	    	// read user name and status line from each tweet and write it to a file
+	        fos.write(Byte.parseByte((status.getUser().getName() + ":" +status.getText())));
 	    }
+	    //closing file
+	    fos.close();
 	}
 
 }
